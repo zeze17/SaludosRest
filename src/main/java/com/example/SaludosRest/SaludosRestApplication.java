559a15486dc369f09008e5d060f1fa644495f0c2
NoJoda <zeze0217@gmail.com>
2022-04-06 22:22:09 -0500

    Primer Commit

---
+++ src/main/java/com/example/SaludosRest/SaludosRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.SaludosRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaludosRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaludosRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SaludosRest/SaludosRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SaludosRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaludosRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
